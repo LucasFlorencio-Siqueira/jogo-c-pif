50c7c9323424fc5ce52ec3ca9baf02ce4cf09879
LucasFlorencio-Siqueira <lhfs2@cesar.school>
2025-12-02 09:22:33 -0300

    Update game.h

--- game.h
+++ game.h
@@ -5,34 +5,25 @@
 #include "fleet.h"
 
 typedef struct {
-    int r, c;
-} Coordenada;
+    Board board;
+    Board shots;
+    Fleet fleet;
+    char nickname[32];
+} Player;
 
 typedef struct {
-    char apelido[30];
-    Tabuleiro* meuTabuleiro;
-    Tabuleiro* mapaTiros;
-    Frota frota;
-    
-    Coordenada* historicoTiros;
-    int totalTiros;
-    int capacidadeHistorico;
-} Jogador;
+    Player p1, p2;
+    int current_player;
+    int game_over;
+} Game;
 
-typedef struct {
-    Jogador j1;
-    Jogador j2;
-    int jogadorAtual;
-    int tamanhoTabuleiro;
-    int fimDeJogo;
-} Jogo;
-
-Jogo* criarJogo(int tam, char* nome1, char* nome2);
-void destruirJogo(Jogo* jogo);
-void inicializarJogador(Jogador* j, char* nome, int tam);
-void posicionarFrotaAuto(Jogador* j);
-EstadoCelula processarTiro(Jogador* atirador, Jogador* alvo, int l, int c);
-void registrarTiro(Jogador* j, int l, int c);
-void alternarTurno(Jogo* jogo);
+Game* criarJogo(int rows, int cols, char* nome1, char* nome2);
+void destruirJogo(Game* game);
+void inicializarJogador(Player* p, char* nome, int rows, int cols);
+void posicionarFrotaAuto(Player* p);
+CellState processarTiro(Player* atirador, Player* defensor, int row, int col);
+void alternarTurno(Game* game);
 
 #endif
+#endif
+
