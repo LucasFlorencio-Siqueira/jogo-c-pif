ac7c71d9426c84163a493c5ff86428a9286f9d44
LucasFlorencio-Siqueira <lhfs2@cesar.school>
2025-12-02 09:12:04 -0300

    Update board.h

--- board.h
+++ board.h
@@ -1,1 +1,28 @@
+#ifndef BOARD_H
+#define BOARD_H
 
+typedef enum {
+    AGUA,
+    NAVIO,
+    ACERTO,
+    ERRO
+} EstadoCelula;
+
+typedef struct {
+    EstadoCelula estado;
+    int idNavio;
+} Celula;
+
+typedef struct {
+    int linhas;
+    int colunas;
+    Celula *celulas;
+} Tabuleiro;
+
+Tabuleiro* criarTabuleiro(int linhas, int colunas);
+void destruirTabuleiro(Tabuleiro* tabuleiro);
+void limparTabuleiro(Tabuleiro* tabuleiro);
+Celula* pegarCelula(Tabuleiro* tabuleiro, int linha, int coluna);
+int coordenadaValida(Tabuleiro* tabuleiro, int linha, int coluna);
+
+#endif
