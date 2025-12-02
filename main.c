041ebcb2409a880f9c849c6c581c297766b3bb02
LucasFlorencio-Siqueira <lhfs2@cesar.school>
2025-12-02 09:24:16 -0300

    Update main.c

--- main.c
+++ main.c
@@ -6,10 +6,10 @@
 int main() {
     iniciarAleatorio();
     
-    int tamTabuleiro = 10;
-    char modoPosicionamento = 'A';
-    char nome1[30] = "Jogador 1";
-    char nome2[30] = "Jogador 2";
+    int tamBoard = 10;
+    char modo = 'A';
+    char nome1[32] = "Alice";
+    char nome2[32] = "Bob";
     int sair = 0;
 
     while (!sair) {
@@ -19,36 +19,36 @@ int main() {
             limparTela();
             pedirNomes(nome1, nome2);
             
-            Jogo* jogo = criarJogo(tamTabuleiro, nome1, nome2);
+            Game* jogo = criarJogo(tamBoard, tamBoard, nome1, nome2);
             
-            if (modoPosicionamento == 'A') {
-                posicionarFrotaAuto(&jogo->j1);
-                posicionarFrotaAuto(&jogo->j2);
+            if (modo == 'A') {
+                posicionarFrotaAuto(&jogo->p1);
+                posicionarFrotaAuto(&jogo->p2);
             }
             
-            while (!jogo->fimDeJogo) {
+            while (!jogo->game_over) {
                 limparTela();
-                Jogador* atual = (jogo->jogadorAtual == 1) ? &jogo->j1 : &jogo->j2;
-                Jogador* inimigo = (jogo->jogadorAtual == 1) ? &jogo->j2 : &jogo->j1;
+                Player* atual = (jogo->current_player == 1) ? &jogo->p1 : &jogo->p2;
+                Player* inimigo = (jogo->current_player == 1) ? &jogo->p2 : &jogo->p1;
                 
                 mostrarTelaJogo(atual, inimigo);
                 
-                int l, c;
-                while (!pedirTiro(&l, &c, tamTabuleiro));
+                int r, c;
+                while (!pedirTiro(&r, &c, tamBoard));
                 
-                EstadoCelula res = processarTiro(atual, inimigo, l, c);
+                CellState res = processarTiro(atual, inimigo, r, c);
                 
                 char* nomeNavio = NULL;
-                if (res == ACERTO) {
-                    Celula* cel = pegarCelula(inimigo->meuTabuleiro, l, c);
+                if (res == CELL_HIT) {
+                    Cell* cel = pegarCelula(&inimigo->board, r, c);
                     if (cel) {
-                        nomeNavio = inimigo->frota.listaNavios[cel->idNavio].nome;
+                        nomeNavio = inimigo->fleet.ships[cel->ship_id].name;
                     }
                 }
                 mostrarMensagemTiro(res, nomeNavio);
                 
-                if (verificarFrotaDestruida(&inimigo->frota)) {
-                    jogo->fimDeJogo = 1;
+                if (verificarFrotaDestruida(&inimigo->fleet)) {
+                    jogo->game_over = 1;
                     mostrarVencedor(atual);
                 } else {
                     alternarTurno(jogo);
@@ -57,7 +57,7 @@ int main() {
             destruirJogo(jogo);
             
         } else if (opcao == 2) {
-            configurarJogo(&tamTabuleiro, &modoPosicionamento);
+            configurarJogo(&tamBoard, &modo);
         } else if (opcao == 3) {
             sair = 1;
         }
